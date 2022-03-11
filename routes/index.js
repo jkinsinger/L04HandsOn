29c8837fff86acd741ba8f407ab45f407c7d6cbe
jkinisnger <joy.kinsinger02@gmail.com>
2022-03-11 07:01:54 -0600

    first commit

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+const mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: "Password!",
+  database: 'Sakila'
+})
+
+connection.connect(function(err){
+  if(err){
+    console.error(err.message)
+    return
+  }
+  console.log('Yay! You are connected to the database.')
+})
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
